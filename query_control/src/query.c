7d7b584f1ddb9526456e99f6d819b839032c99bf
sfrydman-utn <sfrydmanbabenco@frba.utn.edu.ar>
2025-10-11 19:16:22 -0300

    Ajustes de QueryControl para enviar y recibir paquetes

--- query_control/src/query.c
+++ query_control/src/query.c
@@ -12,10 +12,7 @@ int main(int argc, char* argv[]) {
     char* path_config = argv[1];
     char* path_query = argv[2];
     uint32_t prioridad = atoi(argv[3]);
-
-
-    saludar("query_control");
-
+    
     //Inicializo las configs de Query Control
     inicializar_configs(path_config);
 
@@ -37,25 +34,17 @@ int main(int argc, char* argv[]) {
     // --- LOG: Conexión con Master ---
     log_info(logger_query, "## Conexión al Master exitosa. IP: %s, Puerto: %d",
              query_configs.ipmaster, query_configs.puertomaster);
-
-     // Concatenar path_query y prioridad en un solo string
-    // Envío de Query
-    char* mensaje_a_enviar = string_from_format("%s|%s", path_query, prioridad);
-    log_info(logger_query, "## Solicitud de ejecución de Query: %s, prioridad: %s",
-             path_query, prioridad);
-
-    if (enviar_mensaje(mensaje_a_enviar, socket_master) <= 0) {
-        log_error(logger_query, "## Error al enviar Query al Master.");
-        free(mensaje_a_enviar);
-        close(socket_master);
-        log_destroy(logger_query);
-        return EXIT_FAILURE;
-    }
-    free(mensaje_a_enviar);
-
     
+    //Serializo el path de la query y la prioridad
+    t_query query = {path_query, prioridad};
+    t_buffer* buffer = serializar_query(&query);
+    t_paquete* paquete = empaquetar_buffer(PAQUETE_QUERY, buffer);
 
-    // Escuchar mensajes del Master
+    //Se lo envío a Master
+    enviar_paquete(socket_master, paquete);
+    log_info(logger_query, "## Solicitud de ejecución de Query: %s, prioridad: %d", path_query, prioridad);
+    destruir_paquete(paquete);
+    
     escuchar_master(socket_master);
 
     close(socket_master);
@@ -65,84 +54,32 @@ int main(int argc, char* argv[]) {
 
 void escuchar_master(int socket_master) {
     while (1) {
-        char* mensaje = recibir_mensaje(socket_master);
-        if (mensaje == NULL) {
+
+        t_paquete* paquete = recibir_paquete(socket_master);
+
+        if (paquete  == NULL) {
             log_error(logger_query, "## Conexión con Master perdida.");
             break;
         }
 
-        if (strncmp(mensaje, "READ", 4) == 0) {
-            char** partes = string_split(mensaje, "|");
-            char* file = NULL;
-            char* tag = NULL;
-            char* contenido = NULL;
-
-            for (int i = 0; partes[i] != NULL; i++) {
-                if (string_starts_with(partes[i], "File:"))
-                    file = partes[i] + strlen("File:");
-                else if (string_starts_with(partes[i], "Tag:"))
-                    tag = partes[i] + strlen("Tag:");
-                else if (string_starts_with(partes[i], "Contenido:"))
-                    contenido = partes[i] + strlen("Contenido:");
-            }
-
-            log_info(logger_query, "## Lectura realizada: File %s:%s, contenido: %s",
-                     file ? file : "?", tag ? tag : "?", contenido ? contenido : "?");
-
-            string_array_destroy(partes);
-        }
-
-        else if (strncmp(mensaje, "END", 3) == 0) {
-            char** partes = string_split(mensaje, "|");
-            char* motivo = partes[1] ? partes[1] : "Finalización desconocida";
-
-            log_info(logger_query, "## Query Finalizada - %s", motivo);
-
-            string_array_destroy(partes);
-            free(mensaje);
-            break;
-        }
-
-        else {
-            log_warning(logger_query, "## Mensaje desconocido recibido del Master: %s", mensaje);
-        }
-
-        free(mensaje);
-    }
-}
-
-int socket_master = crear_conexion(query_configs.ipmaster, string_itoa(query_configs.puertomaster));
-    log_info(logger_query, "## Conexión al Master exitosa. IP: %s, Puerto: %d", query_configs.ipmaster, query_configs.puertomaster);
-
-    //Serializo el path de la query y la prioridad
-    t_query query = {path_query, prioridad};
-    t_buffer* buffer = serializar_query(&query);
-    t_paquete* paquete = empaquetar_buffer(PAQUETE_QUERY, buffer);
-
-    //Se lo envío a Master
-    enviar_paquete(socket_master, paquete);
-    log_info(logger_query, "## Solicitud de ejecución de Query: %s, prioridad: %d", path_query, prioridad);
-
-
-
-    //Queda a la espera de los mensajes de Master
-    while(1){
-        printf("entra al while");
-        t_paquete* mensaje = recibir_paquete(socket_master);
-        switch(mensaje->codigo_operacion){
+        switch(paquete->codigo_operacion){
             case READ:
-                //Información leída por la query recibida
-                t_operacion_query* informacion = deserializar_operacion_query(mensaje->buffer);
-                log_info(logger_query, "## Lectura realizada: File %s:%s, contenido: %s", informacion->file, informacion->tag, informacion->informacion);
+                t_operacion_query* op = deserializar_operacion_query(paquete->buffer);
+                log_info(logger_query, "## READ recibido: File=%s:%s -> Contenido=%s",
+                         op->file, op->tag, op->informacion);
+                destruir_operacion_query(op);
                 break;
 
             case END:
-                //Finalización de la query recibida
-                log_info(logger_query, "## Query Finalizada - Fin de Query");
-                close(socket_master);
-                return 0;
+                char* motivo = deserializar_string(paquete->buffer);
+                log_info(logger_query, "## Query finalizada: %s", motivo);
+                free(motivo);
+                destruir_paquete(paquete);
+                return;
+
+            default:
+                log_warning(logger_query,"## Código de operación desconocido recibido: %d",paquete->codigo_operacion);
+                break;
         }
-    }
- 
-    return 0;
-}
+        }
+        }
